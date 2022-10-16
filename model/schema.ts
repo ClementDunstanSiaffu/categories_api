dbb397667c8a0fd29ac05365f38ce235dab4e9e2
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-16 08:53:55 +0300

    ignore node_modules and env variable

--- model/schema.ts
+++ model/schema.ts
@@ -2,11 +2,9 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-    
     id:{type:String},
     categoryName:{type:String},
     categoryImage:{type:String}
-    
 })
 
 mongoose.model("CATEGORY_SCHEMA",categorySchema);
